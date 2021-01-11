a31bdbf6b1fc010777056002ea2436f749b7dfe1
gy5461 <1900269631@qq.com>
2021-01-11 20:25:39 +0800

    对输入处理进行了进一步封装

--- DremmaEngine/src/priv/dremma/game/event/KeyInputHandler.java
+++ DremmaEngine/src/priv/dremma/game/event/KeyInputHandler.java
@@ -8,57 +8,67 @@ import priv.dremma.game.util.Debug;
 
 /**
  * 键盘输入处理类
+ * 
  * @author guoyi
  *
  */
 public class KeyInputHandler implements KeyListener {
 
-	//为game添加键盘输入监听
+	// 为game添加键盘输入监听
 	public KeyInputHandler(Game game) {
 		game.addKeyListener(this);
 	}
-	
+
 	/**
 	 * 内部类，键
+	 * 
 	 * @author guoyi
 	 *
 	 */
 	public class Key {
-		private int pressedTimes = 0;	//按键次数
-		private boolean isPressed = false;	//是否被按
-		
+		private int pressedTimes = 0; // 按键次数
+		private boolean isPressed = false; // 是否被按
+		public int keyCode;
+
 		public int getPressedTimes() {
 			return pressedTimes;
 		}
-		
+
 		public boolean isPressed() {
 			return isPressed;
 		}
 
-		//触发键
-		public void toggle(boolean isPressed) {
+		// 触发键
+		public void toggle(int keyCode, boolean isPressed) {
+			this.keyCode = keyCode;
 			this.isPressed = isPressed;
-			if(isPressed) {
+			if (isPressed) {
+				Debug.log(Debug.DebugLevel.WARNING, "Key Pressed:" + keyCode);
+			} else {
+				Debug.log(Debug.DebugLevel.WARNING, "Key Released:" + keyCode);
+			}
+
+			if (isPressed) {
 				pressedTimes++;
 			}
 		}
 	}
-	
-	//上下左右虚拟方向键
+
+	// 上下左右虚拟方向键
 	public Key up = new Key();
 	public Key down = new Key();
 	public Key left = new Key();
 	public Key right = new Key();
+	public Key key = new Key();
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		toggleKey(e.getKeyCode(), true);	//按键事件发生时触发键
-		Debug.log(Debug.DebugLevel.INFO, "Key Pressed:" + e.getKeyChar());
+		toggleKey(e.getKeyCode(), true); // 按键事件发生时触发键
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		toggleKey(e.getKeyCode(), false);	//释放键事件发生时将本键是否被按压属性设为false
+		toggleKey(e.getKeyCode(), false); // 释放键事件发生时将本键是否被按压属性设为false
 	}
 
 	@Override
@@ -67,17 +77,25 @@ public class KeyInputHandler implements KeyListener {
 	}
 
 	public void toggleKey(int keyCode, boolean isPressed) {
+		boolean flag = false;
 		if (keyCode == KeyEvent.VK_W || keyCode == KeyEvent.VK_UP) {
-			up.toggle(isPressed);
+			flag = true;
+			up.toggle(keyCode, isPressed);
 		}
 		if (keyCode == KeyEvent.VK_S || keyCode == KeyEvent.VK_DOWN) {
-			down.toggle(isPressed);
+			flag = true;
+			down.toggle(keyCode, isPressed);
 		}
 		if (keyCode == KeyEvent.VK_A || keyCode == KeyEvent.VK_LEFT) {
-			left.toggle(isPressed);
+			flag = true;
+			left.toggle(keyCode, isPressed);
 		}
 		if (keyCode == KeyEvent.VK_D || keyCode == KeyEvent.VK_RIGHT) {
-			right.toggle(isPressed);
+			flag = true;
+			right.toggle(keyCode, isPressed);
+		}
+		if (!flag) {
+			key.toggle(keyCode, isPressed);
 		}
 	}
 
--- DremmaEngine/src/priv/dremma/game/event/MouseInputHandler.java
+++ DremmaEngine/src/priv/dremma/game/event/MouseInputHandler.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseListener;
 
 import priv.dremma.game.Game;
 import priv.dremma.game.util.Debug;
+import priv.dremma.game.util.Vector2;
 
 /**
  * 鼠标输入处理类
@@ -14,36 +15,77 @@ import priv.dremma.game.util.Debug;
  */
 
 public class MouseInputHandler implements MouseListener {
-	
+
 	public MouseInputHandler(Game game) {
 		game.addMouseListener(this); // 给游戏窗体添加鼠标监听器
 	}
 
-	//鼠标按键单击（按下并释放）时触发，测试时感觉不够灵敏
+	/**
+	 * 内部类，鼠标
+	 * 
+	 * @author guoyi
+	 *
+	 */
+	public class Mouse {
+		private int pressedTimes = 0; // 鼠标按压次数
+		private boolean isPressed = false; // 鼠标是否被按
+		private Vector2 location; // 鼠标按压位置
+
+		public int getPressedTimes() {
+			return pressedTimes;
+		}
+
+		public boolean isPressed() {
+			return isPressed;
+		}
+
+		public Vector2 getLocation() {
+			return location;
+		}
+
+		// 触发键
+		public void toggle(Vector2 location, boolean isPressed) {
+			this.location = location;
+			this.isPressed = isPressed;
+			if (isPressed) {
+				Debug.log(Debug.DebugLevel.WARNING, "Mouse pressed " + location);
+			} else {
+				Debug.log(Debug.DebugLevel.WARNING, "Mouse released " + location);
+			}
+			if (isPressed) {
+				pressedTimes++;
+			}
+		}
+	}
+
+	public Mouse mouse = new Mouse();
+
+	// 鼠标按键单击（按下并释放）时触发，测试时感觉不够灵敏
 	@Override
 	public void mouseClicked(MouseEvent e) {
 
 	}
 
-	//鼠标按键按下时触发
+	// 鼠标按键按下时触发
 	@Override
 	public void mousePressed(MouseEvent e) {
-		Debug.log(Debug.DebugLevel.WARNING, "Mouse pressed X=" + e.getX() + ", Y=" + e.getY());
+		mouse.toggle(new Vector2(e.getX(), e.getY()), true);
+		;
 	}
 
-	//鼠标释放时触发
+	// 鼠标释放时触发
 	@Override
 	public void mouseReleased(MouseEvent e) {
-
+		mouse.toggle(new Vector2(e.getX(), e.getY()), false);
 	}
 
-	//鼠标进入时或进入状态中触发
+	// 鼠标进入时或进入状态中触发
 	@Override
 	public void mouseEntered(MouseEvent e) {
 
 	}
 
-	//鼠标从Enter状态中退出时触发
+	// 鼠标从Enter状态中退出时触发
 	@Override
 	public void mouseExited(MouseEvent e) {
 
